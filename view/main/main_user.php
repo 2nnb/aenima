0e08184f43009c145597ccd518cf632b6e0524f6
B3n0n <b3n0ns@gmail.com>
2013-10-13 22:41:47 +0100

    Update main_user.php
    fixed chat box

--- view/main/main_user.php
+++ view/main/main_user.php
@@ -9,16 +9,18 @@
              <?php        
              }   
         ?>
-       <div id="face_box" class="row">
-          <ul id="msg_list"></ul> 
+       <div class="row">
+        <div id="face_box" class="rounded_top">
+          <ul id="msg_list" class="list-group"></ul>
+        </div>  
        </div>
-       <div id="face_btn_box" class="row">
-        <div id="face_inner_btn_box" class="col-xs-12 col-sm-6 col-md-8 pull-right">
+       <div id="face_btn_box" class="row rounded_bottom">
+        <div id="face_inner_btn_box" class="col-xs-12 col-sm-6 col-md-8 pull-right rounded_right">
           <div class="input-group">            
             <input id="chat_input" type="text" class="form-control"/>
             <div class="input-group-btn">
               <button id="chat_send" type="button" class="btn btn-default" tabindex="-1">Send</button>
-              <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" tabindex="-1">
+              <button type="button" class="btn btn-default dropdown-toggle" disabled="disabled" data-toggle="dropdown" tabindex="-1">
               <span class="caret"/>
               </button>
               <ul class="dropdown-menu" role="menu">
