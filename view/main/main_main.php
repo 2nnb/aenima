10270acf35ec91d18f48d653e73e1825c825e696
B3n0n <b3n0ns@gmail.com>
2013-10-13 19:04:30 +0100

    Update main_main.php

--- view/main/main_main.php
+++ view/main/main_main.php
@@ -1,1 +1,4 @@
-<h1>One great big festering neon distraction</h1>
+<div class="row">
+  <div class="col-xs-12 col-sm-6 col-md-8"><h3>One great big festering neon distraction</h3></div>
+  <div class="col-xs-6 col-sm-6 col-md-4"><p></p></div>
+</div>
